c414c08110cfb609d176e3091d0c9d09b66ae5b6
MariaFerMouro <156363770+MariaFerMouro@users.noreply.github.com>
2024-07-25 14:13:25 -0300

    Create ex003.py

---
+++ ex003.py
@@ -0,0 +1,4 @@
+# Crie um script Python que leia dois números e tente mostrar a soma entre eles
+x = int(input('Digite um número: '))
+y = int(input('outro número: '))
+print(f'A soma de {x} com {y} é: {x+y}')
