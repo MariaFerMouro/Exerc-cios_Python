96df6fc8f391e96698a63caafc9a3091b64f8b9d
MariaFerMouro <156363770+MariaFerMouro@users.noreply.github.com>
2024-07-25 14:11:05 -0300

    Create ex002.py

---
+++ ex002.py
@@ -0,0 +1,5 @@
+# Crie um script Python que leia o dia, o mês e o ano de nascimento de uma pessoa e mostre uma mensagem com a data formatada
+dia = int(input('Digite o dia do seu nascimento: '))
+mes = int(input('Mês: '))
+ano = int(input('Ano: '))
+print(f'Data de nascimento: {dia}/{mes}/{ano}')
